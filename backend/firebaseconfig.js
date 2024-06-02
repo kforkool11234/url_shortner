fd5fdf7374acb0aa092b35bff3f2ad1d7ba9c1e1
kaushal sengupta <kaushalsubho2005@gmail.com>
2024-06-02 21:13:31 +0530

    firebase

---
+++ backend/firebaseconfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh4DDs0h6TR9m6B2EfFF7PvFsgTcSg288",
+  authDomain: "url-shortner-40f36.firebaseapp.com",
+  projectId: "url-shortner-40f36",
+  storageBucket: "url-shortner-40f36.appspot.com",
+  messagingSenderId: "154310512922",
+  appId: "1:154310512922:web:411de23557460037f79ca3",
+  measurementId: "G-RVJB068WYC"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, GoogleAuthProvider };
