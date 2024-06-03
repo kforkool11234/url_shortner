b5b9384a82937787b7218a8656cb19206f5bd631
kaushal sengupta <kaushalsubho2005@gmail.com>
2024-06-03 15:40:51 +0530

    firebaseconfig

--- backend/firebaseconfig.js
+++ backend/firebaseconfig.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider} from 'firebase/auth';
 import dotenv from "dotenv"
 dotenv.config()
 const firebaseConfig = {
-  apiKey: process.env.fireapikey,
-  authDomain: process.env.fireauthDomain,
-  projectId: process.env.fireprojectId,
-  storageBucket: process.env.firestorageBucket,
-  messagingSenderId: process.env.firemessagingSenderId,
-  appId:process.env.fireappId ,
-  measurementId: process.env.firemeasurementId
+  apiKey: "AIzaSyCh4DDs0h6TR9m6B2EfFF7PvFsgTcSg288",
+  authDomain: "url-shortner-40f36.firebaseapp.com",
+  projectId: "url-shortner-40f36",
+  storageBucket: "url-shortner-40f36.appspot.com",
+  messagingSenderId: "154310512922",
+  appId: "1:154310512922:web:411de23557460037f79ca3",
+  measurementId: "G-RVJB068WYC"
 };
 
 const app = initializeApp(firebaseConfig);
